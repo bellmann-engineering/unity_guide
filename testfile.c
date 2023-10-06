3afb07f9eec801408d126387fbe0d4e164089293
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2023-10-06 10:54:32 +0200

    Add files via upload

---
+++ testfile.c
@@ -0,0 +1,40 @@
+#include "unity.h"
+
+// Global variables for sharing data between setup, test, and teardown
+int a;
+int b;
+int result;
+
+// The function to test
+int add(int a, int b) {
+    return a + b;
+}
+
+// This function will be called before each test case
+void setUp(void) {
+    a = 10;
+    b = 5;
+}
+
+// This function will be called after each test case
+void tearDown(void) {
+    // You can add teardown logic here if needed
+}
+
+// Define a test case
+void test_simple_addition() {
+    int expected = 15;
+
+    result = add(a, b);
+
+    TEST_ASSERT_EQUAL_INT(expected, result);
+}
+
+int main() {
+    UNITY_BEGIN();
+
+    // Run the test case
+    RUN_TEST(test_simple_addition);
+
+    return UNITY_END();
+}
